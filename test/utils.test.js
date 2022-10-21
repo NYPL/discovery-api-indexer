0f0c8d9dc50eaa9428a9231cf0f2f139daa89ff5
Paul Beaudoin <contact@paulbeaudoin.com>
2022-10-21 14:02:35 -0400

    Add item date and volume fields
    Adds:
     - item.volumeRange
     - item.volumeRaw
     - item.dateRange
     - item.dateRaw
     - item.enumerationChronology_sort
     - item.type
     - item.format
     - bib.numItemVolumesParsed
     - bib.numItemDatesParsed
    
    https://jira.nypl.org/browse/SCC-3097

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const utils = require('../lib/utils')
+const expect = require('chai').expect
 
 describe('Utils', function () {
   describe('deepFilterByKey', function () {
@@ -123,6 +124,47 @@ describe('Utils', function () {
 
     it('should apply char folding', function () {
       assert.equal(titleSortTransform('naïve'), 'naive')
+    })
+  })
+
+  describe('isArrayWithValues', function () {
+    it('should pass valid arrays', function () {
+      expect(utils.isArrayWithValues([1, 1])).to.equal(true)
+      expect(utils.isArrayWithValues([[1, 1]])).to.equal(true)
+      expect(utils.isArrayWithValues([[1, 1], [2, 3]])).to.equal(true)
+      expect(utils.isArrayWithValues([[[1, 1]], [2, 3]])).to.equal(true)
+    })
+
+    it('should fail invalid arrays', function () {
+      expect(utils.isArrayWithValues()).to.equal(false)
+      expect(utils.isArrayWithValues([])).to.equal(false)
+      expect(utils.isArrayWithValues([[]])).to.equal(false)
+      expect(utils.isArrayWithValues([[], []])).to.equal(false)
+      expect(utils.isArrayWithValues([[[]], [], [[]]])).to.equal(false)
+      expect(utils.isArrayWithValues([[], [[2]]])).to.equal(false)
+      expect(utils.isArrayWithValues([[], [2]], 1)).to.equal(false)
+      expect(utils.isArrayWithValues(1)).to.equal(false)
+    })
+  })
+
+  describe('leftPad', function () {
+    it('left pads to specified length', function () {
+      expect(utils.leftPad('', 8)).to.equal('00000000')
+      expect(utils.leftPad('', 2)).to.equal('00')
+      expect(utils.leftPad('what', 8)).to.equal('0000what')
+    })
+
+    it('left pads with specified char', function () {
+      expect(utils.leftPad('', 8, ' ')).to.equal('        ')
+      expect(utils.leftPad('what', 8, ' ')).to.equal('    what')
+      expect(utils.leftPad('what', 8, '*')).to.equal('****what')
+    })
+
+    it('stringifies non-string inputs', function () {
+      expect(utils.leftPad(undefined, 3, ' ')).to.equal('   ')
+      expect(utils.leftPad(null, 3, ' ')).to.equal('   ')
+      expect(utils.leftPad(10, 3, ' ')).to.equal(' 10')
+      expect(utils.leftPad(true, 5, ' ')).to.equal(' true')
     })
   })
 })
